5598b594266147bbabb87050587714791e2b517d
sujit01111 <144442649+sujit01111@users.noreply.github.com>
2025-04-21 18:05:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "my-cache-v1";
+const CACHE_NAME = 'star-gym-cache-v1';
 const urlsToCache = [
-  "/",
+ "/",
   "/index.html",
-  "html/blog.html",
+  "html/blogs.html",
   "html/class.html",
   "html/products.html",
   "js/app.js",
@@ -12,68 +12,61 @@ const urlsToCache = [
   "style/products.css",
   "style/desktop-view.css",
   "style/mobile-view.css",
-"images/alina-chernysheva.webp",
-"images/Anais-Louis.webp",
-"images/anastase-marago.webp",
-"images/Beginner-Level.webp",
-"images/biceps.webp",
-"images/bruce-mars.webp",
-"images/couple-training.webp",
-"images/cycling.webp",
-"images/dylan-gillis.webp",
-"images/fitness_logo.webp",
-"images/fitness-club.webp",
-"images/fitnish-media.webp",
-"images/ft.webp",
-"images/gm.webp",
-"images/gym-kit.webp",
-"images/hamburger.webp",
-"images/heidi-erickson.webp",
-"images/hypertrophy.webp",
-"images/james.webp",
-"images/John-Smith.webp",
-"images/keagan-henman.webp",
-"images/logo.webp",
-"images/logo1.webp",
-"images/mark-adriane.webp",
-"images/MB-STRAWBERRY.webp",
-"images/Mixed-Level-Excello.webp",
-"images/Multivitamins.webp",
-"images/pexels-bamboo.webp",
-"images/pexels-leon.webp"
+  "images/alina-chernysheva.jpeg",
+"images/Anais-Louis.jpeg",
+"images/anastase-marago.jpeg",
+"images/Beginner-Level.jpeg",
+"images/biceps.png",
+"images/bruce-mars.jpeg",
+"images/couple-training.jpg",
+"images/cycling.png",
+"images/dylan-gillis.jpeg",
+"images/fitness-club.jpeg",
+"images/fitness_logo.png",
+"images/fitnish-media.jpg",
+"images/ft.png",
+"images/gm.png",
+"images/gym-kit.jpeg",
+"images/hamburger.png",
+"images/heidi-erickson.jpeg",
+"images/hypertrophy.png",
+"images/james.jpeg",
+"images/John-Smith.jpeg",
+"images/keagan-henman.jpeg",
+"images/logo.png",
+"images/logo1.png",
+"images/mark-adriane.jpeg",
+"images/MB-STRAWBERRY.jpeg",
+"images/Mixed-Level-Excello.png",
+"images/Multivitamins.jpeg",
+"images/pexels-bamboo.jpeg",
+"images/pexels-leon.jpg",
+"images/pexels-taryn.jpeg",
+"images/pexels-tima.jpeg",
+"images/pg.jpeg",
+"images/Phil-heath.jpeg",
+"images/PowerExcello.jpeg",
+"images/preworkout.jpeg",
+"images/running.png",
+"images/training.jpeg",
+"images/weight-pound.png",
+"images/yoga.png"
+
 ];
 
-// Install Event: Cache assets
-self.addEventListener("install", (event) => {
+// Install event
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Caching assets");
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch Event: Serve cached assets
-self.addEventListener("fetch", (event) => {
+// Fetch event
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Activate Event: Clear old caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log("Deleting old cache:", cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
